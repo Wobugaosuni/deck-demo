594daa2a795dfd7ef93c4ab7c6a95203a49bf5b4
Wobugaosuni <397401374@qq.com>
2019-07-04 16:54:05 +0800

    iconLayers

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,12 +1,14 @@
 /**
  * LineLayer: https://deck.gl/#/documentation/deckgl-api-reference/layers/line-layer
+ * IconLayer: https://deck.gl/#/documentation/deckgl-api-reference/layers/icon-layer
  * tooltip: http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
  */
 
 import React from 'react'
-import {LineLayer} from '@deck.gl/layers'
+import {LineLayer, IconLayer} from '@deck.gl/layers'
 import {StaticMap} from 'react-map-gl'
-import data from './sample'
+import data from './sample-line'
+import data2 from './sample-icon'
 
 import DeckGL, {
   COORDINATE_SYSTEM,
@@ -32,10 +34,27 @@ const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYW5uaHVhbmciLCJhIjoiY2p4bXZqbTc2MDgyaDNo
 //   bearing: 0
 // }
 
+const ICON_MAPPING = {
+  marker1: {
+    x: 0, 
+    y: 0, 
+    width: 128, 
+    height: 128, 
+    mask: false // icon是否使用自定义的颜色(getColor)
+  },
+  marker2: {
+    x: 128, 
+    y: 128, 
+    width: 128, 
+    height: 128, 
+    mask: false // icon是否使用自定义的颜色(getColor)
+  },
+}
+
 const initialViewState = {
   longitude: -122.41669,
   latitude: 37.7853,
-  zoom: 9,
+  zoom: 10,
   pitch: 0,
   bearing: 0
 }
@@ -76,12 +95,40 @@ class App extends React.Component {
       })
     ]
 
+    const layers2 = [
+      new IconLayer({
+        id: 'icon-layer',
+        data: data2,
+        pickable: false,
+        // iconAtlas and iconMapping are required
+        // getIcon: return a string
+        iconAtlas: 'data/location-icon-atlas.png',
+        iconMapping: ICON_MAPPING, // 图标集的定义
+        getIcon: d => { // 展示的图标
+          if (d.entries > 10000) {
+            return 'marker1'
+          }
+          return 'marker2'
+        },
+        sizeScale: 15, // Icon size multiplier
+        getSize: d => 3, // The height of each object, in units specified by sizeUnits (default pixels).
+        getPosition: d => d.coordinates,
+        getColor: d => [Math.sqrt(d.exits), 140, 0],
+        onHover: ({object, x, y}) => {
+          // const tooltip = `${object.name}\n${object.address}`;
+          /* Update tooltip
+            http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
+          */
+        }
+      })
+    ]
+
     return (
       <React.Fragment>
         <DeckGL
           initialViewState={initialViewState}
           controller={true}
-          layers={layers} // 图层，一条竖线
+          layers={layers2} // 图层，一条竖线
         >
           <MapView id="map" width="1200px" height="800px" controller={true}>
             <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
